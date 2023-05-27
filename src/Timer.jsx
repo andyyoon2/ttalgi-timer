6fc6dfa16ecd78b8cf6dc2ad2418171b2b32cebb
Andy Yoon <insoo.yoon@proton.me>
2023-05-26 19:02:00 -0700

    Countdown functionality

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -10,6 +10,7 @@ LONG_BREAK_READY
 
 export default function Timer() {
   const [time, setTime] = createSignal(0);
+  const [timerInterval, setTimerInterval] = createSignal(null);
   const [timeDisplay, setTimeDisplay] = createSignal('Start');
   const [timerState, setTimerState] = createSignal('DONE');
 
@@ -20,17 +21,52 @@ export default function Timer() {
   });
 
   function start() {
-    if (timerState === 'counting') {
+    if (timerState === 'COUNTDOWN') {
       return;
     }
-    setTimerState('counting');
-    setTime(25 * 60);
+    setTimerState('COUNTDOWN');
+    // setTime(25 * 60);
+    setTime(5);
+    startTimer();
+  }
+
+  function startTimer() {
+    if (timerInterval()) {
+      return;
+    }
+    tick();
+  }
+
+  function tick() {
+    // TODO: Calculate drift to get most accurate timer
+    setTimerInterval(
+      setTimeout(() => {
+        setTime(time() - 1);
+        if (time() === 0) {
+          resetTimer();
+          return;
+        }
+        tick();
+      }, 1000)
+    );
+  }
+
+  function pauseTimer() {
+    clearTimeout(timerInterval());
+    setTimerInterval(null);
+  }
+
+  function resetTimer() {
+    setTimerState('DONE');
+    pauseTimer();
+    setTime(0);
   }
 
   return (
     <div>
       <button onClick={start}>Start</button>
       <h2>{timeDisplay()}</h2>
+      {/* <p>{timerState()}</p> */}
     </div>
   );
 }
